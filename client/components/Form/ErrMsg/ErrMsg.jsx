b690a85fc16d4f403e64b13d603ff8eba8cbd39c
Fady-Alwazir <fady.alwazir@gmail.com>
2022-05-16 22:07:54 +0300

    create generic input component with formik Relates #15

--- client/components/Form/ErrMsg/ErrMsg.jsx
+++ client/components/Form/ErrMsg/ErrMsg.jsx
@@ -1,29 +1,30 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 
-function ErrMsg({ children }) {
+function ErrMsg({ error, visible }) {
+  if (!visible || !error) return null;
   return (
     <View style={styles.container}>
-      <Text style={styles.errMsgText}>{children}</Text>
+      <Text style={styles.errMsgText}>{error}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: "80%",
-    height: "6%",
-    backgroundColor: "#fef0ef",
-    borderColor: "#ff3334",
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+    height: '6%',
+    backgroundColor: '#fef0ef',
+    borderColor: '#ff3334',
     borderWidth: 1,
     borderRadius: 5,
   },
   errMsgText: {
     fontSize: 18,
-    textAlign: "center",
-    color: "#ff3334",
+    textAlign: 'center',
+    color: '#ff3334',
     margin: 10,
   },
 });
