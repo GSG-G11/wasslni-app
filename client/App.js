bc403c856db99360d0036282ddca690b15507b60
mayar-kabaja <kabajamayar@gmail.com>
2022-05-16 14:03:20 +0300

    create button component Relates #6

--- client/App.js
+++ client/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Button } from './components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.fixToText}>
+      <Button title='click' color='#ca011a'/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#',
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
 });
---
+++ client/components/UI/index.js
@@ -0,0 +1,3 @@
+import Button from "./Button/Buttom";
+
+export {Button}
