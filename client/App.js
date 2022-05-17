81e7b131baa917313708eaa6a108b55c6fed4424
Fady-Alwazir <fady.alwazir@gmail.com>
2022-05-17 14:59:00 +0300

    Create User Context Relates #22

--- client/App.js
+++ client/App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import UserContext from './context/userContext';
 
 export default function App() {
+  const [user, setUser] = useState({
+    username: '',
+    phoneNumber: '',
+    image: '',
+    lat: 0.0,
+    lng: 0.0,
+    isSeller: false,
+    isLoggedIn: false,
+    id: 0,
+  });
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View>
+    <UserContext.Provider value={{ user, setUser }}>
+      <View style={styles.container}></View>
+    </UserContext.Provider>
   );
 }
 
@@ -16,5 +28,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
- 
 });
