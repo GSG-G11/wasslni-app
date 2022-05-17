49c1bfbe606ef13b44b9e723ffb38df817ea7d6d
unknown <montherismail90@gmail.com>
2022-05-18 02:18:48 +0300

    fixing

--- client/App.js
+++ client/App.js
@@ -1,16 +1,16 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import UserContext from './context/userContext';
-import { Home, Login } from './screens';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import UserContext from "./context/userContext";
+import { Home, Login } from "./screens";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   const [user, setUser] = useState({
-    username: '',
-    phoneNumber: '',
-    image: '',
+    username: "",
+    phoneNumber: "",
+    image: "",
     lat: 0.0,
     lng: 0.0,
     isSeller: false,
@@ -21,11 +21,11 @@ export default function App() {
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="ابدأ" component={Home} />
-        <Stack.Screen name="تسجيل الدخول" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ابدأ" component={Home} />
+          <Stack.Screen name="تسجيل الدخول" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </UserContext.Provider>
   );
 }
@@ -33,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
