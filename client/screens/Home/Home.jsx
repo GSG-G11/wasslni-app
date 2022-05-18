2afe0c9575d510783089ae27076ad4652d8bee24
mayar-kabaja <kabajamayar@gmail.com>
2022-05-18 12:23:37 +0300

    fix style in home page

--- client/screens/Home/Home.jsx
+++ client/screens/Home/Home.jsx
@@ -3,20 +3,22 @@ import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button } from "../../components";
-import homePage from "../../assets/homePage.jpg";
+import homeRes from "../../assets/homeRes.png";
 
 export default function Home({ navigation }) {
   const handleNavigate = () => {
     navigation.navigate("تسجيل الدخول");
   };
   return (
-    <ImageBackground style={styles.homeContainer} source={homePage}>
+    <ImageBackground style={styles.homeContainer} source={homeRes}>
       <View style={styles.homeTextContainer}>
         <Text style={styles.homeText}>
           تطبيق مختص لتوصيل الطرود لكافة محافظات قطاع غزة , بطريقة آمنة ووقت
           قياسي
         </Text>
-      <Button title="ابدأ" onPress={handleNavigate} style={styles.homeButton}/>
+        <View style={styles.homeButton}>
+            <Button title="ابدأ" onPress={handleNavigate} />
+        </View>
       </View>
     </ImageBackground>
   );
@@ -25,20 +27,24 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
   homeContainer: {
     flex: 1,
-    alignItems: "flex-start",
+    alignItems: "flex-end",
     justifyContent: "center",
     width: "100%",
   },
   homeTextContainer: {
       width : "60%",
       paddingLeft: "8%",
+      textAlign: "rigth",
+
   },
   homeText: {
-    textAlign: "left",
-    fontSize: 18,
-    marginBottom: 40,
+    textAlign: "center",
+    fontSize: 16,
+    marginBottom: 100,
   },
   homeButton: {
-    width: "70%",
+    direction:"rtl",
+    marginLeft:10,
+    
   },
 });
