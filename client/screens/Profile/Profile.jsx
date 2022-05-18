3572cf87ca39f110e2d33fc0ac7da5efa5c7dca8
unknown <montherismail90@gmail.com>
2022-05-18 18:48:32 +0300

    part of code Relates #34

--- client/screens/Profile/Profile.jsx
+++ client/screens/Profile/Profile.jsx
@@ -1,42 +1,52 @@
 import React, { useContext } from 'react';
 import UserContext from '../../context/userContext';
-import { Image, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { Map } from '../../components';
 
 function Profile() {
   const { user } = useContext(UserContext);
-    console.log('====================================');
-    console.log(user);
-    console.log('====================================');
   return (
-    <View>
-      <View>
-        <View>
+      <>
+    <View style={styles.container}>
           <View>
             <Image source={user.urlImg} alt="profile" />
+            <Text>Image</Text>
           </View>
-          <View>
-            <Text>
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoText}>
                 <Text>الإسم :</Text>
               <Text>{user.userName}</Text>
             </Text>
-            <Text>
+            <Text style={styles.infoText}>
              <Text> الصلاحيات :</Text>
               <Text>{user.role ? ' تاجر ' : 'مشتري'}</Text>
             </Text>
-            <Text>
+            <Text style={styles.infoText}>
                 <Text>رقم الجوال :</Text>
               <Text>{user.phoneNumber}</Text>
             </Text>
           </View>
-        </View>
         <Text>موقعي</Text>
-        <View>
-          <Map setPosition />
-        </View>
       </View>
-    </View>
+    <Map showPosition />
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    infoContainer: {
+        flex:1,
+        justifyContent: "space-around",
+        alignItems: "center",
+    },
+    infoText: {
+        fontSize:25,
+    }
+});
 
 export default Profile;
