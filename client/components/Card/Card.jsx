08de92f68a22c83c8cfde7a379b63d4f0ab16f46
mayar-kabaja <kabajamayar@gmail.com>
2022-05-18 11:21:30 +0300

    Create Card Component

---
+++ client/components/Card/Card.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { View, Text , StyleSheet} from 'react-native';
+import { Button } from '..';
+
+export default function Card({name, status , id , navigation }) {
+  const onPress = ()=>{
+    navigation.navigate('ParcelDatails', {id : {id}})
+  }
+  return (
+    <View id={id} style={styles.card}>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.status}>{status ? <Text>تم الاستلام</Text> :<Text>لم يتم الاستلام</Text>} </Text>
+      <View style={styles.btn}><Button title="تفاصيل الطرد" onPress={onPress}/></View>
+    </View>
+    
+  );
+}
+const styles = StyleSheet.create({
+  card:{
+    flex: 1,
+    backgroundColor:'#fff',
+    margin:10,
+    padding:10,
+    justifyContent:'center',
+    alignItems: 'flex-end',
+    borderRadius:15,
+    borderRightColor:'#ca011a',
+    borderRightWidth:3,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  name:{
+    fontSize:24,
+    margin:5,
+  },
+  status:{
+    fontSize:18,
+    margin:5,
+  },
+  btn:{
+    fontSize:14,
+  }
+
+})
