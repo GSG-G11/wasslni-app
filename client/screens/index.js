97090be94c02c549be2e361d408bf791fec5b560
unknown <montherismail90@gmail.com>
2022-05-19 12:12:26 +0300

    create parcel details Relates #40

--- client/screens/index.js
+++ client/screens/index.js
@@ -2,5 +2,6 @@ import Home from "./Home/Home";
 import Login from "./Login/Login";
 import Profile from "./Profile/Profile";
 import Parcels from "./Parcels/Parcels";
+import ParcelDetails from "./ParcelDetails/ParcelDetails";
 
-export {Login, Home, Profile, Parcels};
+export {Login, Home, Profile, Parcels, ParcelDetails};
